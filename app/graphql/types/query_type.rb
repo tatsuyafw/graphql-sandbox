fee92abcd3fbb0364e1215326dd8ad3f60210622
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2019-12-19 23:55:45 +0900

    Define a query root

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,16 +1,22 @@
 module Types
   class QueryType < Types::BaseObject
+    description "The query root of this schema"
 
-    field :post, PostType, null: false
+    field :post, PostType, null: true do
+      description "Find a post by ID"
+      argument :id, ID, required: true
+    end
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    def post(id:)
+      Post.find(id)
+    end
 
-    # TODO: remove me
-    # field :test_field, String, null: false,
-    #   description: "An example field added by the generator"
-    # def test_field
-    #   "Hello World!"
-    # end
+    field :posts, [PostType], null: true do
+      description "All posts"
+    end
+
+    def posts
+      Post.all
+    end
   end
 end
