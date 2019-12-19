cd46b099a467065ade6d0e48bdf80b739f4940ab
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2019-12-20 00:18:48 +0900

    Add an error hander for ActiveRecord::RecordNotFound

--- app/graphql/graphql_sandbox_schema.rb
+++ app/graphql/graphql_sandbox_schema.rb
@@ -1,4 +1,11 @@
 class GraphqlSandboxSchema < GraphQL::Schema
+  rescue_from(Exception) do |error|
+    case error
+    when ActiveRecord::RecordNotFound
+      GraphQL::ExecutionError.new(error.message)
+    end
+  end
+
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
